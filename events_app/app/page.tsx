dc0ff1aa48122ac357b164142b46d3028a35a405
Hamza Ashfaq <your-email@example.com>
2025-07-16 11:16:48 +0500

    Initial commit

--- events_app/app/page.tsx
+++ events_app/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-    return <h1>Welcome Home!</h1>
+// import { HeroSection } from "@/components/blocks/HeroSection";
+import { getHomePage } from "@/data/loaders";
+import { notFound } from "next/navigation";
+async function loader() {
+    const data = await getHomePage();
+    if (!data) notFound();
+    console.log(data);
+    return { ...data.data };
+}
+
+export default async function Home() {
+    const data = await loader();
+    const blocks = data?.blocks || [];
+    console.log(data);
+    return (
+    <div>
+     <h1>{data.Title}</h1>
+     <p>{data.description}</p>
+     <HeroSection {...blocks[0]} />
+    </div>
+ );
 }
